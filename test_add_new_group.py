86131ef9ecdc41e3d9b281929520342312134bd7
VladimirBerg <vovabergg@gmail.com>
2024-01-21 17:45:13 +0300

    Создание вспомогательных методов и передача параметров в методы

--- test_add_new_group.py
+++ test_add_new_group.py
@@ -19,22 +19,46 @@ class TestLoginAddGroup():
         self.driver.quit()
 
     def test_login_add_group(self):
-        self.driver.get("http://localhost/addressbook/")
-        self.driver.set_window_size(1070, 759)
-        self.driver.find_element(By.NAME, "user").click()
-        self.driver.find_element(By.NAME, "user").send_keys("admin")
-        self.driver.find_element(By.NAME, "pass").click()
-        self.driver.find_element(By.NAME, "pass").send_keys("secret")
+        self.open_home_page()
+        self.set_window_size()
+        self.login("admin", "secret")
+        self.open_groups_page()
+        self.create_new_group("Group", "Header", "Comment")
+        self.return_to_groups()
+        self.logout()
+        self.driver.close()
+
+    def logout(self):
+        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_groups(self):
+        self.driver.find_element(By.LINK_TEXT, "group page").click()
+
+    def create_new_group(self, name, header, footer):
+        # create new group
+        self.driver.find_element(By.NAME, "new").click()
+        # fill group form
+        self.driver.find_element(By.NAME, "group_name").click()
+        self.driver.find_element(By.NAME, "group_name").send_keys(name)
+        self.driver.find_element(By.NAME, "group_header").click()
+        self.driver.find_element(By.NAME, "group_header").send_keys(header)
+        self.driver.find_element(By.NAME, "group_footer").click()
+        self.driver.find_element(By.NAME, "group_footer").send_keys(footer)
+        # submit
+        self.driver.find_element(By.NAME, "submit").click()
+
+    def open_groups_page(self):
         self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
         self.driver.find_element(By.LINK_TEXT, "groups").click()
-        self.driver.find_element(By.NAME, "new").click()
-        self.driver.find_element(By.NAME, "group_name").click()
-        self.driver.find_element(By.NAME, "group_name").send_keys("Group2")
-        self.driver.find_element(By.NAME, "group_header").click()
-        self.driver.find_element(By.NAME, "group_header").send_keys("Header")
-        self.driver.find_element(By.NAME, "group_footer").click()
-        self.driver.find_element(By.NAME, "group_footer").send_keys("Comment")
-        self.driver.find_element(By.NAME, "submit").click()
-        self.driver.find_element(By.LINK_TEXT, "group page").click()
-        self.driver.find_element(By.LINK_TEXT, "Logout").click()
-        self.driver.close()
+
+    def login(self, username, password):
+        self.driver.find_element(By.NAME, "user").click()
+        self.driver.find_element(By.NAME, "user").send_keys(username)
+        self.driver.find_element(By.NAME, "pass").click()
+        self.driver.find_element(By.NAME, "pass").send_keys(password)
+
+    def set_window_size(self):
+        self.driver.set_window_size(1070, 759)
+
+    def open_home_page(self):
+        self.driver.get("http://localhost/addressbook/")
