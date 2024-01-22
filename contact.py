553140f5dba8bd95ca6f9b7750a89ce4882d1047
VladimirBerg <vovabergg@gmail.com>
2024-01-22 10:57:08 +0300

    Занятие 1. Задание 3

---
+++ contact.py
@@ -0,0 +1,18 @@
+class Info:
+    def __init__(self, firstname, middlename, lastname, nickname, company, address, mobile, email, new_group):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.new_group = new_group
+
+
+class Birthday:
+    def __init__(self, bday, bmonth, byear):
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
