5c608bfc29a8ea24c988225f7a3228b8d03fc2fb
VladimirBerg <vovabergg@gmail.com>
2024-02-15 22:30:00 +0300

    Добавлены проверки для содержания списков групп

--- test/test_group_delete.py
+++ test/test_group_delete.py
@@ -8,4 +8,5 @@ def test_first_group_delete(app):
     app.group.delete_first()
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
-
+    old_groups[0:1] = []
+    assert old_groups == new_groups
--- test/test_group_edit.py
+++ test/test_group_edit.py
@@ -5,6 +5,10 @@ def test_group_edit(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group_for_edit"))
     old_groups = app.group.get_group_list()
-    app.group.edit(Group(name="Group123"))
+    group = Group(name="Group123")
+    group.id = old_groups[0].id
+    app.group.edit(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
