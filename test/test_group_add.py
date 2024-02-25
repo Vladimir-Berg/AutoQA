aeedae5e46cd1d6b0a5a43072aa267491c8f983b
VladimirBerg <vovabergg@gmail.com>
2024-02-25 15:25:18 +0300

    Реализовано хеширование для групп

--- test/test_group_add.py
+++ test/test_group_add.py
@@ -5,7 +5,7 @@ def test_login_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group("Group", "Header", "Comment")
     app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
