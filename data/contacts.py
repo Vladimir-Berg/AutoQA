fd1e4b23ef2c93caa07577bf0da7d9329a22d73e
VladimirBerg <vovabergg@gmail.com>
2024-03-07 00:43:00 +0300

    Задание 18

---
+++ data/contacts.py
@@ -0,0 +1,10 @@
+from model.contact import Contact
+
+testdata = [
+    Contact("Vladimir1", "Vova-mid", "Berg", "Vova-nick", "CompStyleee", "Street 5 Home 6",
+            "8-666-555-77-44", "8(666)5557755", "8(666)5557766",
+            "email@gmail.com", "email@gmail.com", "email@gmail.com", "26", "September", 2000),
+    Contact("Vladimir2", "Vova-mid", "Berg", "Vova-nick", "CompStyleee", "Street 5 Home 6",
+            "8-666-555-77-44", "8(666)5557755", "8(666)5557766",
+            "email@gmail.com", "email@gmail.com", "email@gmail.com", "26", "September", 2000)
+]
