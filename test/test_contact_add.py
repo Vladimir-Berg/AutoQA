ea7c40f5efded9081d8b1f221149dd2758a8c5e0
Vladimir Berg <vladimir.berg@lenvendo.ru>
2024-05-22 11:46:24 +0300

    Соединение с БД

--- test/test_contact_add.py
+++ test/test_contact_add.py
@@ -1,17 +1,11 @@
 from model.contact import Contact
 
 
-def test_addcontact(app, json_contacts):
+def test_add_contact(app, json_contacts):
     contact = json_contacts
     old_contacts = app.contact.get_contacts_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
-    new_contants = app.contact.get_contacts_list()
+    new_contacts = app.contact.get_contacts_list()
     old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contants, key=Contact.id_or_max)
-
-
-"""def test_addcontact_1(app):
-    app.contact.create(
-        Contact("vova", "family", "last", "nick", "comp-2", "adr", "86665557744", "dfdf", "26",
-                "September", 2000))"""
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_contact_delete.py
+++ test/test_contact_delete.py
@@ -13,6 +13,6 @@ def test_contact_delete(app):
     app.contact.delete_contact_by_index(index)
     app.driver.implicitly_wait(7)
     assert len(old_contacts) - 1 == app.contact.count()
-    new_contants = app.contact.get_contacts_list()
+    new_contacts = app.contact.get_contacts_list()
     old_contacts[0:1] = []
-    assert old_contacts == new_contants
+    assert old_contacts == new_contacts
