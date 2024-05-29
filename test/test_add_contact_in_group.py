427f2ce445ecfd16f763ef0df758fac12ac9bbb4
Vladimir Berg <vladimir.berg@lenvendo.ru>
2024-05-29 09:14:51 +0300

    Задание 22. Поиск пустых групп

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -15,12 +15,28 @@ def test_add_contact_in_group(app, db):
     if len(db.get_groups_list()) == 0:
         app.group.create(Group("Group1", "Header1", "Footer1"))
 
+    groups_list = app.contact.get_groups_from_list()
+
+    empty_groups = check_empty_groups(db, groups_list)
+    if len(empty_groups) == 0:
+        group = Group('1', '1', '1')
+        app.group.create(group)
+        groups_list = app.contact.get_groups_from_list()
+        empty_groups = check_empty_groups(db, groups_list)
+
+    group = random.choice(empty_groups)
     old_contacts = db.get_contact_list()
     contact = random.choice(old_contacts)
-    groups_list = app.contact.get_groups_from_list()
-    group = random.choice(groups_list)
     old_contacts_in_group = db.get_contacts_in_group(group)
     app.contact.add_contact_in_group(contact.id, group)
     new_contacts_in_group = db.get_contacts_in_group(group)
     old_contacts_in_group.append(contact)
     assert sorted(old_contacts_in_group, key=Contact.id_or_max) == sorted(new_contacts_in_group, key=Contact.id_or_max)
+
+
+def check_empty_groups(db, groups_list):
+    empty = []
+    for item in groups_list:
+        if len(db.get_contacts_in_group(item)) == 0:
+            empty.append(item)
+    return empty
