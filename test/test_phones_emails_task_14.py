8e45d35ba4c72cc7c6293da16576ce73006fcb82
Vladimir Berg <vladimir.berg@lenvendo.ru>
2024-06-04 12:06:27 +0300

    Задание 14. Правки

---
+++ test/test_phones_emails_task_14.py
@@ -0,0 +1,33 @@
+import re
+
+
+def test_phones_on_home_page(app):
+    contact_from_home_page = app.contact.get_contacts_list()[1]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(1)
+    assert contact_from_home_page.all_phones_on_home_page == merge_phone_from_edit_page(contact_from_edit_page)
+    assert contact_from_home_page.all_emails_on_home_page == merge_emails_from_edit_page(contact_from_edit_page)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.address == contact_from_edit_page.address
+
+
+def clear_phones(s):
+    return re.sub("[-, ()]", "", s)
+
+
+def clear_emails(s):
+    return re.sub("[ ]", "", s)
+
+
+def merge_phone_from_edit_page(contact):
+    return '\n'.join(filter(lambda x: x != '',
+                            map(lambda x: clear_phones(x),
+                                filter(lambda x: x is not None,
+                                       [contact.homephone, contact.mobilephone, contact.workphone]))))
+
+
+def merge_emails_from_edit_page(contact):
+    return '\n'.join(filter(lambda x: x != '',
+                            map(lambda x: clear_emails(x),
+                                filter(lambda x: x is not None,
+                                       [contact.email1, contact.email2, contact.email3]))))
